15bf3460a71f54a71fbfb1e2fad391a37247a213
Mridual-code <mridual.14910@stu.upes.ac.in>
2026-07-03 12:11:53 +0530

    built models

---
+++ backend/models/Blog.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+
+    content: {
+      type: String,
+      required: true
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    category: {
+      type: String,
+      required: true
+    },
+
+    status: {
+      type: String,
+      enum: ["Draft", "Pending", "Approved", "Rejected"],
+      default: "Draft"
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
